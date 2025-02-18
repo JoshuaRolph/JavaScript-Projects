2f9696d3e24be6c75315b999fcee3ec3df0f40ee
JoshuaRolph <jrolph45@gmail.com>
2025-02-18 16:25:19 -0700

    Update main.js

--- Project1_expressions_alert/main.js
+++ Project1_expressions_alert/main.js
@@ -1,3 +1,9 @@
-<script>
-alert("Hello, World!");
-</script>
+var Family ="Rolph", Dad="Joshua", Mom="Caitlyn", Daughter="Hannah", Daughter="Elizabeth", Son="Liam";
+
+document.write (Dad);
+
+
+function My_First_Function() {                                      //Defining my First Function
+    var str = "This is the button text!";                           //Assigning a Variable
+    document.getElementById("Button_Text").innerHTML = str;         //Putting value of Variable into HTML elementFromPoint with the "Button_Text" id
+}
