005943531cc037417a1cff8fd91ea1ce0cd25c4b
JoshuaRolph <jrolph45@gmail.com>
2025-02-18 17:00:18 -0700

    Update main.js

--- Project1_expressions_alert/main.js
+++ Project1_expressions_alert/main.js
@@ -1,9 +1,18 @@
-var Family ="Rolph", Dad="Joshua", Mom="Caitlyn", Daughter="Hannah", Daughter="Elizabeth", Son="Liam";
-
-document.write (Dad);
-
-
 function My_First_Function() {                                      //Defining my First Function
     var str = "This is the button text!";                           //Assigning a Variable
     document.getElementById("Button_Text").innerHTML = str;         //Putting value of Variable into HTML elementFromPoint with the "Button_Text" id
 }
+
+//window.alert method
+window.alert ("Its An Alert");
+//document.write method
+document.write("Another Alert");
+//Making 2 Variables
+var Sent1 = "Very first String";
+var Sent2 = "That continues";
+
+//Adding the 2 variables together
+document.write(Sent1 + Sent2);
+
+//A mathematical equation
+document.write(90+20*2);
