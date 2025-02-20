d447a35c399e94eae89b80c13bdc4526282d2efd
JoshuaRolph <jrolph45@gmail.com>
2025-02-19 17:54:07 -0700

    Update main.js

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,1 +1,10 @@
-const elem = document.getElementById("first")
+
+//---Concatenate Function---//
+function myFunction() {
+    var sentence = "I learn";
+    sentence += " Stuff";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
+function myFunction() {
+    document.getElementById("demo").innerHTML = "OMG ITS A BUTTON!";
+  }
