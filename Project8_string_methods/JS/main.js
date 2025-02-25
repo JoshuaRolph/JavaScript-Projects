92a287a9959d6f09359aea9d75d5327693bf71d6
JoshuaRolph <jrolph45@gmail.com>
2025-02-24 17:00:51 -0700

    Update main.js

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -1,3 +1,5 @@
+//Concatenate Combine Sentence//
+
 function full_Sentence() {
     var part_1 =" This is ";
     var part_2 =" All About ";
@@ -6,4 +8,27 @@ function full_Sentence() {
     var part_5 =" Full Sentences";
     var whole_sentence = part_1.concat(part_2, part_3, part_4, part_5);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+//Slice Method//
+
+function slice_Method() {
+    var Sentence = "All work and no play makes Jack a Dull boy";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+//ToUppercCase Method//
+
+function myFunction() {
+    let text = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML =
+    text.toUpperCase();
+  }
+
+//String Number Method//
+
+function string_Method() {
+    var X =182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
 }
